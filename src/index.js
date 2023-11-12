27177c08ef737d4c86f74f2bcbf235a5e6c2dae8
Hasnain_Malik <mhicoder@gmail.com>
2023-11-12 23:20:55 +0500

    middleware configuration and api response, error utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import dotenv from 'dotenv'
 import connectDb from './db/db.js'
+import { app } from './app.js'
 
 dotenv.config()
 connectDb()
+  .then(() => {
+    app.on('error', (error) => {
+      console.log('Server is able to connect with database')
+      throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runing at port: ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log('MongoDB connection failed !!!', error)
+  })
